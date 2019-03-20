d5fd03336a1d6250036c0603a4e536fe5d6ba04b
stijnvanhoey <stijnvanhoey@gmail.com>
2019-03-21 00:31:08 +0100

    Update challenge R file

--- src/20190321_challenges.R
+++ src/20190321_challenges.R
@@ -35,8 +35,29 @@ n_crab_provinces <- read_csv("./data/20190321_n_crabs_provinces.csv")
 # https://rstudio.github.io/leaflet/colors.html
 # https://rstudio.github.io/leaflet/legends.html
 
+# YOUR CODE...
 
 
+
+# Challenge 3
+# 1. Add occurrences from 2015 as markers/circles on the map
+# 2. Provide popup
+# 3. make sure the plot focuses on region around coordinate 51.1373384, 4.2813167
+# 4. Add WMS services on rivers in Flanders as optional layers to the map
+
+# Hints:
+# - popups https://stackoverflow.com/questions/36887384/advanced-popups-in-r-shiny-leaflet
+# - layercontrol https://rstudio.github.io/leaflet/showhide.html
+# - wmstiles https://inbo.github.io/tutorials/tutorials/spatial_wms_services/#as-background-of-interactive-maps
+# - look for layer names on https://geoservices.informatievlaanderen.be/raadpleegdiensten/VMM/wms?service=WMS&version=1.3.0&request=GetCapabilities
+
+# read data
+crab_df_2015 <- read_csv("./data/20190321_crab_occurrences_2015.csv")
+
+# define URL WMS service VMM
+wms_vmm <- "https://geoservices.informatievlaanderen.be/raadpleegdiensten/VMM/wms"
+
+# YOUR CODE...
 
 
 
