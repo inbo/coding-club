ba8512e39cdb7c33781b82ad2da71373378b214a
stijnvanhoey <stijnvanhoey@gmail.com>
2019-05-22 23:12:59 +0200

    Add challenge src file

---
+++ src/20190523_challenges.R
@@ -0,0 +1,16 @@
+
+library(DBI)
+library(odbc)
+library(tidyverse)
+
+# Challenge 1: ----
+# Install/ and load the inborutils package
+# Use the `connect_inbo_dbase` function from the inborutils package to
+# connect to the database "W0003_00_Lims" or "D0021_00_userFlora".
+
+# YOUR CODE...
+
+
+
+# Challenge 2: ----
+#
