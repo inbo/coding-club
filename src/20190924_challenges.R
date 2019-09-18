6ecbffbed0321334fd63aed0ff8d0487fe58eb7a
damianooldoni <damiano.oldoni@inbo.be>
2019-09-18 10:39:43 +0200

    Update challenge src file

--- src/20190924_challenges.R
+++ src/20190924_challenges.R
@@ -1,17 +1,45 @@
-library(readr)
+library(tidyverse)
+library(here)
 
-decay_1 <- read_csv("./data/20180821_decay_measurements_1.csv")
 
-# Get summary stats about concentration decay
-min_decay_1 <- min(decay_1$conc_data, na.rm = TRUE) # min
-max_decay_1 <- max(decay_1$conc_data, na.rm = TRUE)
-t_min_decay_1 <- decay_1$time[which.min(decay_1$conc_data)] # time to min
-# d-50
-d_50_decay_1 <- decay_1$time[which.min(abs(max(decay_1$conc_data)/2 - decay_1$conc_data))]
+## CHALLENGE 1
+
+# file path
+area_districts_f <- here("data", "oppervlakte_straat_wijken_Brugge.csv")
+area_districts <- read_delim(area_districts_f, delim = ";")
+
+# minimum area
+min_area <- min(area_districts$OPPERVLAKTE, na.rm = TRUE) # min
+# distict with smallest area
+smallest_district <- area_districts$NAAM[which.min(area_districts$OPPERVLAKTE)]
+
+# maximum area
+max_area <- max(area_districts$OPPERVLAKTE, na.rm = TRUE) # max
+# district with biggest area
+biggest_district <- area_districts$NAAM[which.max(area_districts$OPPERVLAKTE)]
+
+# average area
+average_area <- mean(area_districts$OPPERVLAKTE, na.rm = TRUE) # mean
+# the district with area most similar to average
+average_district <- area_districts$NAAM[which.min(abs(area_districts$OPPERVLAKTE - average_area))]
 
 # summarize these info in data frame (output)
-info_decay_1 <- data.frame(min_decay = min_decay_1,
-                           t_min_decay = t_min_decay_1,
-                           max_decay = max_decay_1,
-                           d_50_time = d_50_decay_1,
-                           stringsAsFactors = FALSE)
+info_districts <- tibble(min_area = min_area,
+                             smallest_district = smallest_district,
+                             max_area = max_area,
+                             biggest_district = biggest_district,
+                             average_area = average_area,
+                             average_district = average_district)
+
+################################################################################
+
+## CHALLENGE 2
+
+# Adapt the function get_info_districts so that you can reuse your function to
+# tackle also "oppervlakte_straat_wijken_Antwerpen.csv" without
+# altering the data itself
+
+
+
+################################################################################
+
