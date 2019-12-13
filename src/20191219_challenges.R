ed9027745a90532b7c47dcb70f3abe0432eb7bbc
damianooldoni <damiano.oldoni@inbo.be>
2019-12-13 17:39:23 +0100

    Add source file for challenges 20121912

---
+++ src/20191219_challenges.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(INBOtheme)
+
+# read survey data
+survey <- read_csv("./data/20180222_surveys.csv")
+# remove records without weight or hindfoot length
+survey <- survey %>%
+  filter(!is.na(weight) & !is.na(hindfoot_length) & !is.na(sex))
+
+# read fit data
+fit_results <- read_csv("./data/20191219_gam_fit_results.csv",
+                        na = "")
+
+## CHALLENGE 1
+
+
+
+## CHALLENGE 2
+# Hint: use egg or cowplot
+# install.packages("egg")
+# install.packages("cowplot")
+
+fit_results_sum <-
+  fit_results %>%
+  group_by(year) %>%
+  summarize(obs = sum(obs, na.rm = TRUE))
+
+# Use fit_results_sum for new plot
+
+
+## INTERMEZZO
+# Play a little with dark themes
+install.packages("ggdark")
+library(ggdark)
+
+
+
+## CHALLENGE 3
+
+install.packages("magick")
+library(magick)
+
+# Option 1
+
+
+
+# Option 2
+
+# Basic version to improve
+ggplot(survey) +
+  geom_point(aes(weight, hindfoot_length, color = sex),
+             alpha = 0.3) +
+  facet_wrap(~year)
