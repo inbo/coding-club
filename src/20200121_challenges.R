d15f32dca56ab0c6d72174f65b2c6178ed702b40
damianooldoni <damiano.oldoni@inbo.be>
2020-01-16 15:07:30 +0100

    Add R script for challenges

---
+++ src/20200121_challenges.R
@@ -0,0 +1,51 @@
+library(tidyverse) # to do datascience
+library(readxl) # to read Excel files
+
+### Challenge 1: data import
+
+# import your dataset
+
+
+# What is the `head()` of your data.frame?
+
+
+# What is the `str()`ucture of your data.frame?
+
+
+# Did you know you can inspect some `summary()` statistics?
+
+
+# Did you know you can get `nrows()` and `ncols()`?
+
+
+
+### Challenge 2: data exploration/transformation
+
+# - Did you know you can know how many `distinct()` values you have?
+
+
+# - Did you know you can `filter()` rows?
+#   1. `count()` how many NAs you have in a column. Tip: use `is.na()`
+
+
+#   2. Filter rows with NAs in a specific column out and assign (`<-`) the result as a new data.frame
+
+
+# - Did you know you can `select()` columns?
+
+
+# - Did you know you can `rename()` a column?
+
+
+# - Calculate `min()`, `max()` and `mean()` of a column
+
+
+
+### Challenge 3: data visualization
+
+# 1. Make two basic plots from your data based on intermezzo
+
+
+# 2. Customize them a little more and make them shining
+
+
