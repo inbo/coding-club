232abad8e63715e58eb960766a60f90b772d48de
damianooldoni <damiano.oldoni@inbo.be>
2020-01-27 15:39:01 +0100

    Improve text in R script

--- src/20200128_challenges.R
+++ src/20200128_challenges.R
@@ -21,28 +21,32 @@ butterflycounts_df <- read_delim("./data/20200128_garden_butterfly_counts.csv", 
 # 1. summarise() as in challenge 2 but now lets' do it for each species
 
 
-# 2. Total number of individuals per species and month
+# 2. Number of individuals per species
+
+
+# 3. Number of individuals per species and month
 
 
 ## BONUS CHALLENGE
 
 # In how many months per year and per garden did countings occur?
-# months_garden <-
+# (months_garden)
 
-# Code for plotting (number of months column is here called n_months)
-p <- ggplot(months_garden, aes(n_months)) +
+# Code for plotting (column with number of months is here called `n_months`)
+ggplot(months_garden, aes(n_months)) +
   geom_histogram(bins = 13, binwidth = 0.75) +
   scale_x_continuous(breaks = seq(0, 13, 1)) +
   xlab("Number of months") +
   ylab("Number of gardens") +
   facet_wrap(~year)
-p
 
 
-# Top month for each species
+# Top month as number of individuals for each species
 
 
 
-# Top 3 months for each species
+# Top 3 months as number of individuals for each species
 
 
+# Species-month combinations of species with at least 4 months with over 2000
+# individuals
