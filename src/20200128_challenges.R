1092c6417e507ac996de9193ca8254354b057095
damianooldoni <damiano.oldoni@inbo.be>
2020-01-27 21:40:51 +0100

    Add code for plots in R script

--- src/20200128_challenges.R
+++ src/20200128_challenges.R
@@ -15,34 +15,58 @@ butterflycounts_df <- read_delim("./data/20200128_garden_butterfly_counts.csv",
 
 
 
-# let's work with `summarize()`
+# let's work with `summarise()`
 
 
 
 ## CHALLENGE 3
 
-# 1. summarise() as in challenge 2 but now lets' do it for each species
+# 1. summarise() as in challenge 2 but now let's do it for each species
 
 
-# 2. Number of individuals per species
+# 2. Number of individuals per species (tot_species)
 
 
-# 3. Number of individuals per species and month
+# plot it! We used `total` for total number of individuals
+ggplot(tot_species, aes(x = species, y = total)) +
+  geom_bar(aes(reorder(species, total)), stat = "identity", fill = "darkred") +
+  xlab("Species") +
+  ylab("Total number of individuals") +
+  theme_bw() +
+  coord_flip()
 
+# 3. Number of individuals per species and month (tot_species_month)
+
+
+
+# plot it! We used `total` for total number of individuals
+# x = species, y = total, facet =  month
+ggplot(tot_species_month, aes(x = species, y = total)) +
+  geom_col() +
+  xlab("Species") +
+  ylab("Number of individuals") +
+  facet_wrap(~month, ncol = 2) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5))
+
+# plot it! x = month, y = total, facet = species
+ggplot(tot_spec_month, aes(x = month, y = tot_individuals)) +
+  geom_col() +
+  xlab("Month") +
+  ylab("Number of individuals") +
+  facet_wrap(~species)
 
 ## BONUS CHALLENGE
 
 # In how many months per year and per garden did countings occur?
 # (months_garden)
 
-# Plot it! (column with number of months is called `n_months`)
+# Plot it! We used column with number of months is called `n_months`
 ggplot(months_garden, aes(n_months)) +
   geom_histogram(bins = 13, binwidth = 0.75) +
   scale_x_continuous(breaks = seq(0, 13, 1)) +
   xlab("Number of months") +
   ylab("Number of gardens") +
   facet_wrap(~year)
-
 
 # Top month as number of individuals for each species
 
@@ -53,3 +77,4 @@ ggplot(months_garden, aes(n_months)) +
 
 # Species-month combinations of species with at least 4 months with over 2000
 # individuals
+
