9480077b946412863aa4662696061bfa79de524e
damianooldoni <damiano.oldoni@inbo.be>
2020-03-24 12:25:36 +0100

    Add R script with start of challenges 20200326

---
+++ src/20200326_challenges.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(sf)
+library(janitor)
+
+# CHALLENGE 1
+
+butterfly_df <- read_csv("data/20200326_butterflies.txt", na = "")
+
+# View first 10 rows for inspection
+butterfly_df %>% head(n = 10) %>% View
+
+
+
+
+## CHALLENGE 2
+
+
+
+
+## CHALLENGE 3
+
+
+
+
+## BONUS CHALLENGE
+
+
