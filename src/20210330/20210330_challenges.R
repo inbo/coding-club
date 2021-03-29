7155affa2f86fd3db92d5827b774c505a905aef0
damianooldoni <damiano.oldoni@inbo.be>
2021-03-29 14:05:35 +0200

    Add comment aobut warnings using read_csv for untidy data

--- src/20210330/20210330_challenges.R
+++ src/20210330/20210330_challenges.R
@@ -8,10 +8,10 @@ library(tidyverse)
 
 ## CHALLENGE 3
 
-
+#' Do you get warnings?
+#' Sure, the number of columns is not constant: data are NOT TIDY!
 camtrap_data <- read_csv("./data/20210330/20210330_camtrap_data.csv",
                          col_names = paste0("V",seq_len(21)))
-
 
 camtrap_data
 
