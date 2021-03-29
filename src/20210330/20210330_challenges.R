86dd511c7432ace59141c75827e2c25e179d689f
hansvancalster <hans.vancalster@inbo.be>
2021-03-29 12:59:20 +0200

    use read_csv

--- src/20210330/20210330_challenges.R
+++ src/20210330/20210330_challenges.R
@@ -8,16 +8,10 @@ library(tidyverse)
 
 ## CHALLENGE 3
 
-#' note you cannot use tidyverse function read_csv(), as fill argument is not
-#' provided. Use standard read.csv() instead
-camtrap_data <- read.csv("./data/20210330/20210330_camtrap_data.csv",
-                         header = FALSE,
-                         fill = TRUE,
-                         col.names = paste0("V",seq_len(21)),
-                         stringsAsFactors = FALSE)
 
-# make camtrap_data a tibble  (= read-friendly data.frame)
-camtrap_data <- as_tibble(camtrap_data )
+camtrap_data <- read_csv("./data/20210330/20210330_camtrap_data.csv",
+                         col_names = paste0("V",seq_len(21)))
+
 
 camtrap_data
 
