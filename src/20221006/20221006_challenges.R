dbd5cb1bdad4bc085b2f5572f19e9682203d4b39
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-10-04 16:24:50 +0200

    Add draft coding club 20221006

---
+++ src/20221006/20221006_challenges.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+
+## CHALLENGE 3
+
+# code to be on top of app.R
+
+# read dataset (path to be checked)
+butterfly_data <- read_csv("./data/20221006/20221006_butterflies_data.txt", na = "")
+# biotopes
+biotopes <- unique(butterfly_data$biotope)
+# species
+sp <- unique(butterfly_data$species)
+
+
+# example of plot for one selected biotope/species combination
+b <- biotopes[1] # Agriculture
+s <- sp[1] # Limenitis camilla
+
+ggplot(butterfly_data %>%
+         filter(species == s,
+                biotope == b),
+         aes(x = year,
+             y = meanArea)) +
+  geom_point() +
+  geom_smooth() +
+  labs(title = "Distribution of Pararge aegeria",
+       y = "Area (%)") +
+  facet_wrap(~ region)
