37c16b8d893ead28a93ef1058adc1eda78c8656c
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-10-10 11:04:53 +0200

    Add solutions 20221006

---
+++ src/20221006_solutions_challenge2/app.R
@@ -0,0 +1,51 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+# CHALLENGE 2
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("butterflies - biotopes"),
+
+    # choice of biotope and species
+    sidebarLayout(
+        sidebarPanel(
+          radioButtons(
+            inputId = "b",
+            label = "biotope",
+            choices = c("Agriculture", "Forest", "Open", "Urban")
+            ),
+          selectInput(
+            inputId = "s",
+            label = "species",
+            choices = c("Limenitis camilla", "Pararge aegeria")
+          )
+        ),
+
+        # Show a table with the selected data
+        mainPanel(
+          textOutput("selected_var")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$selected_var <- renderText({
+    paste("You have selected the biotope", input$b, "and the species", input$s)
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ src/20221006_solutions_challenge3/app.R
@@ -0,0 +1,68 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+
+# CHALLENGE 3
+
+# read dataset (path to be checked)
+butterfly_data <- read_csv("./data/20221006_butterflies_data.txt", na = "")
+# biotopes
+biotopes <- unique(butterfly_data$biotope)
+# species
+sp <- unique(butterfly_data$species)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("butterflies - biotopes"),
+
+    # choice of biotope and species
+    sidebarLayout(
+        sidebarPanel(
+          radioButtons(
+            inputId = "b",
+            label = "biotope",
+            choices = biotopes
+            ),
+          selectInput(
+            inputId = "s",
+            label = "species",
+            choices = sp
+          )
+        ),
+
+        # Show a table with the selected data
+        mainPanel(
+          plotOutput("plot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    ggplot(butterfly_data %>%
+             filter(species == input$s,
+                    biotope == input$b),
+           aes(x = year,
+               y = meanArea)) +
+      geom_point() +
+      geom_smooth() +
+      labs(title = paste("Distribution of ", input$s, "- biotope:", input$b),
+           y = "Area (%)") +
+      facet_wrap(~ region)
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
