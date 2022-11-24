3a43e03c789a0a54278d7a34dd282c78eb752774
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-11-24 22:35:50 +0100

    Update filename natura2000 file 20221124

--- src/20221124/20221124_challenges.R
+++ src/20221124/20221124_challenges.R
@@ -26,7 +26,7 @@ legend
 #' 5. Calculate the most and least occurring land use category in each of the
 #' Natura2000 protected areas (read these areas using the provided code). Tip: use exactextractr::exact_extract() and find
 #' the right [summary operations](https://isciences.gitlab.io/exactextractr/#summary-operations)
-natura2000 <- st_read("./data/20211125/20211125_protected_areas_Lambert72.gpkg",
+natura2000 <- sf::st_read("./data/20221124/20221124_protected_areas_Lambert72.gpkg",
                       layer = "ps_hbtrl")
 
 
