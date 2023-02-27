6de13fc7bb87e8979a795eb27e3dfcd6e0da7904
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-02-27 22:52:49 +0100

    Add R script 20230228

---
+++ src/20230228/20230228_challenges.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+## Read data
+
+partridge <- read_csv("./data/20230228/20230228_partridge.txt", na = "")
+
+# Preview of the data.frame structure
+str(partridge)
+
+## CHALLENGE 1
+
+
+
+
+
+## CHALLENGE 2 - Summaries
+
+
+
+
+## CHALLENGE 3 - Two-table verbs
+
+
+
+## BONUS CHALLENGE 1 - Tidy data
+
+ias <- read_csv("./data/20230228/20230228_ias_plants.txt", na = "")
+head(ias)
+
