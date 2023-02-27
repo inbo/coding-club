d8caa74052fcd8e4c60f8a959196fbd93d5ed2db
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-02-27 23:07:31 +0100

    Add code to R script 20230228

--- src/20230228/20230228_challenges.R
+++ src/20230228/20230228_challenges.R
@@ -20,9 +20,12 @@ str(partridge)
 
 ## CHALLENGE 3 - Two-table verbs
 
+# read the game management units details
+WBE <- read_csv("./data/20230228/20230228_wbe_info.txt", na = "")
 
 
-## BONUS CHALLENGE 1 - Tidy data
+
+## BONUS CHALLENGE - Create tidy data with tidyr
 
 ias <- read_csv("./data/20230228/20230228_ias_plants.txt", na = "")
 head(ias)
