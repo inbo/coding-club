400cabce08e4cd41ec730cb23a8fce47c6d3bf46
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-10-29 08:15:50 +0100

    Add challenges 20241029

---
+++ src/20241029/20241029_challenges.R
@@ -0,0 +1,43 @@
+## CHALLENGE 1
+
+make_bread <- function(grains, yeast, water, salt) {
+  # Code to generate `bread`.
+  # The code here can be easy (easy bread recipes do exist)
+  # or quite complex (complex bread recipes do exist too)
+  bread <- grains + yeast + water + salt
+  return(bread)
+}
+
+make_focaccia <- function(grains, yeast, water, salt) {
+  # Code to generate `focaccia`
+  focaccia <- grains + 1.5 * sqrt(yeast) + 0.7 * water^2 + 2 * salt
+  return(focaccia)
+}
+
+
+
+## Intermezzo 2 - dependencies
+
+make_bread <- function(grains, yeast, water, salt) {
+  assertthat::assert_that(is.scalar(grains))
+  assertthat::assert_that(is.scalar(yeast))
+  assertthat::assert_that(is.scalar(water))
+  assertthat::assert_that(is.scalar(salt))
+  bread <- grains + yeast + water + salt
+  return(bread)
+}
+
+
+
+
+
+
+## CHALLENGE 3B
+
+# Define the dummy data.frame for some ingredients
+ingredients <- data.frame(
+  grains = c(100, 200, 300),
+  yeast = c(1, 2, 3),
+  water = c(200, 300, 400),
+  salt = c(5, 10, 15)
+)
